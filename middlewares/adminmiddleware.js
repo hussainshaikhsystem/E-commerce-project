986836afeb4f25b1af5560c2579ff3db8882f699
hussainshaikhsystem <hussainshaikkh123@gmail.com>
2026-09-22 00:31:29 +0530

    Cleaned root duplicate files

--- middlewares/adminmiddleware.js
+++
@@ -1,9 +0,0 @@
-const usermodel = require("../models/usermodel.js");
-const admin = (req, res, next) => {
-  if (req.user && req.user.role === "admin") {
-    next();
-  } else {
-    res.status(404).json({ message: "access denied, admin only" });
-  }
-};
-module.exports = { admin };
--- models/paymentmodel.js
+++
@@ -1,7 +0,0 @@
-const {  mongoose } = require("mongoose");
-
-const paymentschema = new mongoose.Schema({
-
-})
-const paymentmodel = mongoose.model('payment', paymentschema)
-module.exports = paymentmodel;
