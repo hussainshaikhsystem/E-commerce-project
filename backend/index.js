c8b92b92cc3955a60445fdec37a2277be7f86dca
hussainshaikhsystem <hussainshaikkh123@gmail.com>
2026-09-25 02:51:15 +0530

    fixed .envs

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ connectdb();
 const app = express();
 app.use(cors(
   {
-    origin:   'http://localhost:5173',
+    origin:   process.env.FRONTEND_URL || 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
   }
